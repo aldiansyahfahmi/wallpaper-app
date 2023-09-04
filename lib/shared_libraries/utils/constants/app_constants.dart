8b74496adfab60357e424ea67ca2b9864a0c769c
aldiansyahfahmi <aldiansyahfahmi6@gmail.com>
2023-09-04 21:28:10 +0800

    remove unused code

--- lib/shared_libraries/utils/constants/app_constants.dart
+++ lib/shared_libraries/utils/constants/app_constants.dart
@@ -1,23 +1,8 @@
 class AppConstants {
   const AppConstants();
 
-  static App app = const App();
-  static CachedKey cachedKey = const CachedKey();
   static AppApi appApi = const AppApi();
   static ErrorKey errorKey = const ErrorKey();
-  static ErrorMessage errorMessage = const ErrorMessage();
-}
-
-class App {
-  const App();
-
-  double get defaultMargin => 16.0;
-}
-
-class CachedKey {
-  const CachedKey();
-
-  String get tokenKey => 'tokenKey';
 }
 
 class AppApi {
@@ -35,10 +20,4 @@ class ErrorKey {
   const ErrorKey();
 
   String get message => "message";
-}
-
-class ErrorMessage {
-  const ErrorMessage();
-
-  String get failedGetToken => 'failed get token';
 }
